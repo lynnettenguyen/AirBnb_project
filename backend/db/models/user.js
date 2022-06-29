f61d2a6ab4553f65da7560c96e5f02b568a637ea
lynnettenguyen <nguyelynne@gmail.com>
2022-06-29 10:28:01 -0700

    added User model authentication scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,8 +1,54 @@
 'use strict';
 const { Model, Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    toSafeObject() {
+      // method will return an object with only the User instance information that is safe to save to a JWT, like id, username, and email
+      const { id, username, email } = this;
+      return { id, username, email };
+    };
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString())
+      // returns true if there is a match with the User instance's hashedPassword
+    };
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+      // uses the currentUser scope to return a User with that id
+    };
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        // searches for one User with the specified credential (either a username or an email
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      // if user is found, validate the password using .validatePassword method
+      if (user && user.validatePassword(password)) {
+        // if password is valid, method will return user by using currentUser scope
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    };
+
+    // creates a user with the username, email, and hashedPassword
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    };
+
     static associate(models) {
       // define association here
     }
@@ -36,7 +82,24 @@ module.exports = (sequelize, DataTypes) => {
           len: [60, 60]
         }
       }
+    }, {
+    sequelize,
+    modelName: "User",
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scope: {
+      currentUser: {
+        attributes: {
+          exclude: ['hashedPassword']
+        }
+      },
+      loginUser: {
+        attributes: {}
+      }
     }
-  );
+  });
   return User;
 };

1d1b58a5e026dfc85435b7867cba19a82d8e534f
lynnettenguyen <nguyelynne@gmail.com>
2022-07-23 18:43:15 -0700

    db

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -7,18 +7,28 @@ const HomePage = () => {
   const dispatch = useDispatch()
 
   const allRooms = useSelector(getAllRooms)
-  
+
+  // for (let room of allRooms) console.log(room.previewImage[0].url)
+  // console.log(allRooms[0].previewImage[0])
+
   useEffect(() => {
     dispatch(listAllRooms())
   }, [dispatch])
 
   return (
-    <div>
+    <>
       <h1>HomePage</h1>
-      <div>
-      </div>
-    </div>
+      {allRooms?.map((rooms) => {
+        return (
+          <div>
+            <div>{rooms.name}</div>
+            <div>{rooms.price}</div>
+            <img src={`${rooms.previewImage[0]?.url}`}></img>
+          </div>
+        )
+      })
+      }
+    </>
   )
 }
-
 export default HomePage

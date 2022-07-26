2321a0c7964edc859a0cff5a170ffed414f7beff
lynnettenguyen <nguyelynne@gmail.com>
2022-07-26 00:10:57 -0700

    image size room details

--- frontend/src/components/RoomDetails/index.js
+++ frontend/src/components/RoomDetails/index.js
@@ -26,83 +26,87 @@ const RoomDetails = () => {
 
   return (
     <>
-      <div className="entire-room-page">
-        <div className="room-name">{room?.name}</div>
-        <div className="room-information-top">
-          <span><i class="fa-solid fa-star"></i>{avgStarRating}</span>
-          <span className="span-separator">·</span>
-          <span>{`${room?.numReviews} reviews`}</span>
-          <span className="span-separator">·</span>
-          <span>{`${room?.city}, ${room?.state}, ${room?.country}`}</span>
-        </div>
-        <div className="room-images">
-          <div className="left-image-div">
-            {room?.images &&
-              <img src={room?.images[0]?.url} alt="exterior" className="main-image"></img>}
+      <div className="whole-page">
+        <div className="left-space"></div>
+        <div className="entire-room-page">
+          <div className="room-name">{room?.name}</div>
+          <div className="room-information-top">
+            <span><i class="fa-solid fa-star"></i>{avgStarRating}</span>
+            <span className="span-separator">·</span>
+            <span>{`${room?.numReviews} reviews`}</span>
+            <span className="span-separator">·</span>
+            <span>{`${room?.city}, ${room?.state}, ${room?.country}`}</span>
           </div>
-          <div className="right-image-div">
-            {room?.images?.map((image, i) => {
-              if (i > 0)
-                return (
-                  <div className="side-image-div" key={image.url}>
-                    <img src={`${image?.url}`} alt="interior" className={`side-images side-images${i}`}></img>
-                  </div>
-                )
-            })}
-          </div>
-        </div>
-        <div className="room-information-bottom">
-          <div className="room-description">{room?.description}</div>
-          <div className="reservation-div">
-            <div className="reserve-details">
-              <div className="reserve-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}</div><span>night</span>
-              <span className="reserve-rating"><i class="fa-solid fa-star"></i>{avgStarRating}</span>
-              <span className="reserve-review">· {room?.numReviews} reviews</span>
+          <div className="room-images">
+            <div className="left-image-div">
+              {room?.images &&
+                <img src={room?.images[0]?.url} alt="exterior" className="main-image"></img>}
             </div>
-            <div>
-              <form>
-                <div className="reservation-dates">
-                  <div className="check-in">
-                    <label>CHECK-IN</label>
+            <div className="right-image-div">
+              {room?.images?.map((image, i) => {
+                if (i > 0)
+                  return (
+                    <div className="side-image-div" key={image.url}>
+                      <img src={`${image?.url}`} alt="interior" className={`side-images side-images${i}`}></img>
+                    </div>
+                  )
+              })}
+            </div>
+          </div>
+          <div className="room-information-bottom">
+            <div className="room-description">{room?.description}</div>
+            <div className="reservation-div">
+              <div className="reserve-details">
+                <div className="reserve-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}</div><span>night</span>
+                <span className="reserve-rating"><i class="fa-solid fa-star"></i>{avgStarRating}</span>
+                <span className="reserve-review">· {room?.numReviews} reviews</span>
+              </div>
+              <div>
+                <form>
+                  <div className="reservation-dates">
+                    <div className="check-in">
+                      <label>CHECK-IN</label>
+                      <input
+                        type="date"
+                        className="select-date"
+                        value={new Date(checkIn).toISOString().slice(0, 10)}
+                        onChange={(e) => setCheckIn(new Date(e.target.value).toISOString().slice(0, 10))}
+                      />
+                    </div>
+                    <div className="check-out">
+                      <label>CHECK-OUT</label>
+                      <input
+                        type="date"
+                        className="select-date"
+                        value={new Date(checkOut).toISOString().slice(0, 10)}
+                        onChange={(e) => setCheckOut(new Date(e.target.value).toISOString().slice(0, 10))}
+                      />
+                    </div>
+                  </div>
+                  <div className="guests">
+                    <label>Guests</label>
                     <input
-                      type="date"
-                      className="select-date"
-                      value={new Date(checkIn).toISOString().slice(0, 10)}
-                      onChange={(e) => setCheckIn(new Date(e.target.value).toISOString().slice(0, 10))}
-                    />
+                      type="number"
+                      className="select-guests"
+                      min="1" />
                   </div>
-                  <div className="check-out">
-                    <label>CHECK-OUT</label>
-                    <input
-                      type="date"
-                      className="select-date"
-                      value={new Date(checkOut).toISOString().slice(0, 10)}
-                      onChange={(e) => setCheckOut(new Date(e.target.value).toISOString().slice(0, 10))}
-                    />
-                  </div>
-                </div>
-                <div className="guests">
-                  <label>Guests</label>
-                  <input
-                    type="number"
-                    className="select-guests"
-                    min="1" />
-                </div>
-                <button className="reserve-button">Reserve</button>
-              </form>
-            </div>
-            <div className="total-fees">
-              <div>{room?.price} x # nights</div>
-              <div>$$$$</div>
-              <div>Cleaning Fee</div>
-              <div>$$$$</div>
-              <div>Service Fee</div>
-              <div>$$$$</div>
-              <div>Total before taxes</div>
-              <div>$$$$</div>
+                  <button className="reserve-button">Reserve</button>
+                </form>
+              </div>
+              <div className="total-fees">
+                <div>{room?.price} x # nights</div>
+                <div>$$$$</div>
+                <div>Cleaning Fee</div>
+                <div>$$$$</div>
+                <div>Service Fee</div>
+                <div>$$$$</div>
+                <div>Total before taxes</div>
+                <div>$$$$</div>
+              </div>
             </div>
           </div>
         </div>
+        <div className="right-space"></div>
       </div>
     </>
   )

62e11e51c32856122ea918c35be36d896a48c1f3
lynnettenguyen <nguyelynne@gmail.com>
2022-07-26 09:21:33 -0700

    reviews for home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -26,9 +26,21 @@ const HomePage = () => {
                 <div className="img-div">
                   <img className="room-img" src={`${room?.images[0]?.url}`} alt="preview of room"></img>
                 </div>
-                <div className="room-info">
-                  <p className="room-city-state">{`${room?.city}, ${room?.state}`}</p>
-                  <p className="room-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} night`}</p>
+                <div className="room-detail">
+                  <div className="room-info">
+                    <div className="room-city-state">{`${room?.city}, ${room?.state}`}</div>
+                    <div className="room-price-night">
+                      <div className="room-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}</div><span className="room-night">night</span>
+                    </div>
+                  </div>
+                  <div className="room-rating">
+                    <div className="star-icon">
+                      <i className="fa-solid fa-star"></i>
+                    </div>
+                    <div className="number-rating">
+                      #.#
+                    </div>
+                  </div>
                 </div>
               </div>
             </Link>

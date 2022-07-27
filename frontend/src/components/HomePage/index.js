f140d74ee4c7b5f12913fa68b214be44942b0726
lynnettenguyen <nguyelynne@gmail.com>
2022-07-26 23:11:41 -0700

    homepage sizing

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -17,49 +17,53 @@ const HomePage = () => {
   }, [])
 
   return (
-    <>
-      <div className="all-rooms-div">
-        {allRooms?.map((room, i) => {
+    <div className="all-rooms-page">
+      <div></div>
+      <div>
+        <div className="all-rooms-div">
+          {allRooms?.map((room, i) => {
 
-          let sum = 0;
-          const reviews = room?.Reviews
-          for (let review of reviews) sum += review.stars
-          let avgStars = sum / Object.values(reviews).length || 2
-          avgStars = Math.round(avgStars * 100) / 100
+            let sum = 0;
+            const reviews = room?.Reviews
+            for (let review of reviews) sum += review.stars
+            let avgStars = sum / Object.values(reviews).length || 2
+            avgStars = Math.round(avgStars * 100) / 100
 
-          const wholeNumbers = [1, 2, 3, 4, 5]
-          if (wholeNumbers.includes(avgStars)) avgStars = avgStars.toString() + ".0"
+            const wholeNumbers = [1, 2, 3, 4, 5]
+            if (wholeNumbers.includes(avgStars)) avgStars = avgStars.toString() + ".0"
 
-          return (
-            <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
-              <div className={`room-div room-div${i}`}>
-                <div className="img-div">
-                  <img className="room-img" src={`${room?.images[0]?.url}`} alt="preview of room"></img>
-                </div>
-                <div className="room-detail">
-                  <div className="room-info">
-                    <div className="room-city-state">{`${room?.city}, ${room?.state}`}</div>
-                    <div className="room-price-night">
-                      <div className="room-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}</div>
-                      <span className="room-night">night</span>
+            return (
+              <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
+                <div className={`room-div room-div${i}`}>
+                  <div className="img-div">
+                    <img className="room-img" src={`${room?.images[0]?.url}`} alt="preview of room"></img>
+                  </div>
+                  <div className="room-detail">
+                    <div className="room-info">
+                      <div className="room-city-state">{`${room?.city}, ${room?.state}`}</div>
+                      <div className="room-price-night">
+                        <div className="room-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}</div>
+                        <span className="room-night">night</span>
+                      </div>
+                    </div>
+                    <div className="room-rating">
+                      <div className="star-icon">
+                        <i className="fa-solid fa-star"></i>
+                      </div>
+                      <div className="number-rating">
+                        {avgStars}
+                      </div>
                     </div>
                   </div>
-                  <div className="room-rating">
-                    <div className="star-icon">
-                      <i className="fa-solid fa-star"></i>
-                    </div>
-                    <div className="number-rating">
-                      {avgStars}
-                    </div>
-                  </div>
                 </div>
-              </div>
-            </Link>
-          )
-        })
-        }
+              </Link>
+            )
+          })
+          }
+        </div>
       </div>
-    </>
+      <div></div>
+    </div>
   )
 }
 export default HomePage

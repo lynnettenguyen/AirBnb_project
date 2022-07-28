7e8707d273f77d95c6ab4fe5e72d0732cc5b24f6
lynnettenguyen <nguyelynne@gmail.com>
2022-07-28 13:49:30 -0700

    reservation page

--- frontend/src/components/UserReservations/index.js
+++ frontend/src/components/UserReservations/index.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { findUserReservation, getAllReservations, removeReservation, updateReservation } from "../../store/reservations";
 import "./UserReservations.css"
+import ReserveRoom from "../ReserveRoom";
 
 const UserReservations = () => {
   const sessionUser = useSelector(state => state.session.user);
@@ -50,37 +51,59 @@ const UserReservations = () => {
 
   return (
     <>
-      <h1>Trips</h1>
+      <h2>Trips</h2>
+      <h3>Upcoming Reservations</h3>
       <div className="trips-left-div"></div>
       <div className="trips-main-div">
         {trips?.map((reservation, i) => {
           return (
-            <>
-              <div key={reservation.id}>
-                <div>room: {reservation?.roomId}</div>
-                <div>reservation: {reservation.id}</div>
-                <div>{reservation?.Room?.name}</div>
-                <div>start: {reservation?.startDate}</div>
-                <div>end: {reservation?.endDate}</div>
-                <div>
+            <div key={reservation.id} className="main-reservation div">
+              <div className="left-res-content">
+                <div className="top-left-res-content">
+                  <div className="top-left-name">
+                    <div>{reservation?.Room?.name}</div>
+                  </div>
+                  <div className="top-left-delete">
+                    <button onClick={handleDelete(reservation.id)} className="res-delete">Delete</button>
+                  </div>
+                </div>
+                <div className="bottom-left-res-content">
+                  <div className="bottom-location">
+                    <div>{reservation?.Room?.address}</div>
+                    <div>{`${reservation?.Room?.city}, ${reservation?.Room?.state}`}</div>
+                    <div>{reservation?.Room?.country}</div>
+                  </div>
+                  <div className="bottom-change-res">
+                    <div className="bottom-edit-res">
+                      <button type="button" onClick={() => { setRoomId(reservation?.roomId); setCheckIn(reservation?.startDate); setCheckOut(reservation?.endDate); setReservationId(reservation?.id); setEditReservation(!editReservation) }}>Edit</button>
+                    </div>
+                    <div className="bottom-dates">
+                      <div>start: {reservation?.startDate}</div>
+                      <div>end: {reservation?.endDate}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="middle-change-res">
+                <section>
                   {editReservation ? <form onSubmit={handleSubmit}>
                     <div className="reservation-dates">
-                      <div className="check-in">
-                        <label className="check-label">CHECK-IN</label>
+                      <div className="res-check-in">
+                        <label className="res-check-label">CHECK-IN</label>
                         <input
                           type="date"
                           min={new Date().toISOString().split('T')[0]}
-                          className="select-date"
+                          className="res-select-date"
                           value={new Date(checkIn).toISOString().slice(0, 10)}
                           onChange={(e) => setCheckIn(new Date(e.target.value).toISOString().slice(0, 10))}
                         />
                       </div>
-                      <div className="check-out">
-                        <label className="check-label">CHECKOUT</label>
+                      <div className="res-check-out">
+                        <label className="res-check-label">CHECKOUT</label>
                         <input
                           type="date"
                           min={new Date().toISOString().split('T')[0]}
-                          className="select-date"
+                          className="res-select-date"
                           value={new Date(checkOut).toISOString().slice(0, 10)}
                           onChange={(e) => setCheckOut(new Date(e.target.value).toISOString().slice(0, 10))}
                         />
@@ -90,12 +113,14 @@ const UserReservations = () => {
                       </div>
                     </div>
                   </form> : <></>}
-                  <button type="button" onClick={() => { setRoomId(reservation?.roomId); setCheckIn(reservation?.startDate); setCheckOut(reservation?.endDate); setReservationId(reservation?.id); setEditReservation(!editReservation) }}>Edit</button>
-                  <button onClick={handleDelete(reservation.id)}>Delete</button>
+                </section>
+              </div>
+              <div className="right-res-content">
+                <div className="right-image-res">
+                  <img className="res-img" src={`${reservation?.Room?.images[0]?.url}`}></img>
                 </div>
               </div>
-              <br></br>
-            </>
+            </div>
           )
         })}
       </div>

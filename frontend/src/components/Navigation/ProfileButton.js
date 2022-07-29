5b7caf4e89423cd5325ed0766ace20957f98e8e3
lynnettenguyen <nguyelynne@gmail.com>
2022-07-29 12:50:39 -0700

    no bookings

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -49,7 +49,6 @@ function ProfileButton({ user }) {
   //   }
   // }, [showMenu])
 
-
   return (
     <>
       <div>
--- frontend/src/components/UserReservations/index.js
+++ frontend/src/components/UserReservations/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { listAllReservations, getAllReservations, removeReservation, updateReservation } from "../../store/reservations";
 import "./UserReservations.css"
@@ -18,15 +18,12 @@ const UserReservations = () => {
   const [showEdit, setShowEdit] = useState(false)
   const [reservationErrors, setReservationErrors] = useState([])
   const [checkDates, setCheckDates] = useState(true)
-  const [cancel, setCancel] = useState(true)
 
   const reservationsPerRoom = allReservations.filter(reservation => reservation.roomId === roomId && sessionUser.id !== reservation.userId)
   const trips = allReservations.filter(reservation => sessionUser.id === reservation.userId)
 
   const allStartDates = reservationsPerRoom.map(reservation => reservation.startDate)
   const allEndDates = reservationsPerRoom.map(reservation => reservation.endDate)
-
-  console.log(cancel)
 
   useEffect(() => {
     dispatch(listAllReservations())
@@ -35,7 +32,6 @@ const UserReservations = () => {
 
     if (new Date() > new Date(checkIn)) {
       errors.push("Cannot modify previous reservations")
-      setCancel(false)
     }
 
     if (checkIn === checkOut)
@@ -103,6 +99,22 @@ const UserReservations = () => {
       <div className="trips-left-div"></div>
       <div className="trips-main-div">
         <div className="trips-header">Trips</div>
+        <div class="main-no-trips">
+          <div className="outer-no-trips">
+            <div className="wave-icon">
+              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', height: '58px', width: '58px', fill: 'rgb(227, 28, 95)', stroke: 'currentcolor' }} width={58} height={58}><g stroke="none" fill="#E31C5F"><path d="M15.629 22.596l-2.735 2.801a2 2 0 0 1-2.792.07L7.554 22.67c-.73 2.89-1.162 4.807-1.295 5.75-.134.942-.213 1.72-.238 2.334-.005.238.013.6.056 1.086.17 1.21.515 2.33 1.011 3.333 1.825 3.69 5.47 5.748 8.949 5.869 3.31.115 5.517-.794 8.313-3.48l2.715-2.752-11.436-12.214z" opacity=".2" fill="#E31C5F" /><path d="M28.207 9.793c.469.468.79 1.028.965 1.622l.62-.622a3.828 3.828 0 0 1 5.755 5.026 3.829 3.829 0 0 1 1.81 6.23l-1.77 1.78c.452.133.885.351 1.272.655l.348.309a3.828 3.828 0 0 1 .154 5.252l-10.437 10.56c-1.044.935-1.74 1.522-2.086 1.76-1.884 1.375-3.787 2.15-6.1 2.464-.723.155-1.868.196-3.432.123-7.054-.575-12.678-6.198-13.257-13.25-.146-2.892.572-6.85 2.153-11.876 1.019-3.917 1.793-6.789 2.323-8.616.239-1.315 2.137-1.414 3.72-.754l.327.15c1.867.933 2.87 2.808 2.462 5.299l-.735 4.381L22.793 9.793a3.828 3.828 0 0 1 5.414 0zm-3.877 1.302L12.836 22.578c4.186 4.427 4.186 11.502-.204 16.054l-1.414-1.414c3.64-3.642 3.708-9.504.153-13.28L9.93 22.343l1.09-6.54c.351-1.752-.204-2.84-1.341-3.409-.34-.18-.777-.286-1.31-.317-1.986 7.282-3.228 11.911-3.726 13.886-.422 1.887-.634 3.556-.634 5.01.235 6.32 5.165 11.443 11.405 11.98 1.127.058 2.14.024 3.039-.104 1.998-.271 3.588-.919 5.221-2.11.613-.33 4.653-4.311 12.12-11.946a1.828 1.828 0 0 0-2.463-2.698l-6.057 6.045-1.362-1.467 9.882-9.88a1.829 1.829 0 0 0 .203-2.345l-.203-.24a1.828 1.828 0 0 0-2.586 0l-9.785 9.784-1.363-1.467 11.734-11.732a1.829 1.829 0 0 0 .203-2.345l-.203-.24a1.829 1.829 0 0 0-2.463-.113L19.57 23.844l-1.362-1.467 8.586-8.584a1.829 1.829 0 0 0 .112-2.463l-.235-.235a1.829 1.829 0 0 0-2.34 0zM47 17v2h-5v-2h5zM42.293 4.293l1.414 1.414-4 4-1.414-1.414 4-4zM31 1v5h-2V1h2z" fill="#E31C5F" /></g></svg>
+            </div>
+            <div className="no-trips-header">No trips booked...yet!
+            </div>
+            <div className="no-trips-caption">Time to dust off your bags and start planning your next adventure</div>
+            <div className="no-trips-search-button">
+              <Link to="/">
+              <button type="button" className="start-search-button">Start searching</button>
+              </Link>
+            </div>
+            <div></div>
+          </div>
+        </div>
         <div className="reservation-header">Upcoming Reservations</div>
         <form onSubmit={handleSubmit}>
           {trips?.map((reservation, i) => {
@@ -142,7 +154,7 @@ const UserReservations = () => {
                             <div className="res-year">{endYear}</div>
                           </div>
                           <div className="bottom-edit-res">
-                            <button type="button" onClick={() => { setReservationId(reservation?.id); setRoomId(reservation?.roomId); setCheckIn(reservation?.startDate); setCheckOut(reservation?.endDate); setEditReservation(reservation?.id); setShowEdit(!showEdit); setCancel(false) }} className="res-button">{showEdit ? editReservation === reservation.id ? "X" : "Edit" : "Edit"}</button>
+                            <button type="button" onClick={() => { setReservationId(reservation?.id); setRoomId(reservation?.roomId); setCheckIn(reservation?.startDate); setCheckOut(reservation?.endDate); setEditReservation(reservation?.id); setShowEdit(!showEdit); }} className="res-button">{showEdit ? editReservation === reservation.id ? "X" : "Edit" : "Edit"}</button>
                           </div>
                         </div>
                         <div className="bottom-location">
@@ -165,50 +177,50 @@ const UserReservations = () => {
                       <div className={new Date() > new Date(checkIn) ? "hidden" : "update-res-header"}>Update reservation:</div>
                       <div className={new Date() > new Date(checkIn) ? "hidden" : "middle-change-res"}>
                         <div className={new Date() > new Date(checkIn) ? "hidden" : "reservation-dates-res"}>
-                        <div className="check-res" disabled>
-                          <label className="check-label">CHECK-IN</label>
-                          <input
-                            type="date"
-                            min={new Date().toISOString().split('T')[0]}
-                            className="select-date-res"
-                            value={new Date(checkIn).toISOString().slice(0, 10)}
-                            onChange={(e) => setCheckIn(new Date(e.target.value).toISOString().slice(0, 10))}
-                          />
+                          <div className="check-res" disabled>
+                            <label className="check-label">CHECK-IN</label>
+                            <input
+                              type="date"
+                              min={new Date().toISOString().split('T')[0]}
+                              className="select-date-res"
+                              value={new Date(checkIn).toISOString().slice(0, 10)}
+                              onChange={(e) => setCheckIn(new Date(e.target.value).toISOString().slice(0, 10))}
+                            />
+                          </div>
+                          <div className="check-res">
+                            <label className="check-label">CHECKOUT</label>
+                            <input
+                              type="date"
+                              min={new Date().toISOString().split('T')[0]}
+                              className="select-date-res"
+                              value={new Date(checkOut).toISOString().slice(0, 10)}
+                              onChange={(e) => setCheckOut(new Date(e.target.value).toISOString().slice(0, 10))}
+                            />
+                          </div>
                         </div>
-                        <div className="check-res">
-                          <label className="check-label">CHECKOUT</label>
-                          <input
-                            type="date"
-                            min={new Date().toISOString().split('T')[0]}
-                            className="select-date-res"
-                            value={new Date(checkOut).toISOString().slice(0, 10)}
-                            onChange={(e) => setCheckOut(new Date(e.target.value).toISOString().slice(0, 10))}
-                          />
+                        <div>
+                          <div className="edit-delete-buttons">
+                            <button type="submit" className="res-button update-button" disabled={checkDates}>Update Reservation</button>
+                            <button type="button" onClick={handleDelete(reservation.id)} className="res-button cancel-button" disabled={new Date() > new Date(checkIn)}>Cancel Reservation</button>
+                          </div>
                         </div>
                       </div>
-                      <div>
-                        <div className="edit-delete-buttons">
-                          <button type="submit" className="res-button update-button" disabled={checkDates}>Update Reservation</button>
-                          <button type="button" onClick={handleDelete(reservation.id)} className="res-button cancel-button" disabled={new Date() > new Date(checkIn)}>Cancel Reservation</button>
-                        </div>
-                      </div>
-                    </div>
                       {reservationErrors.length > 0 && (
-                  <>
-                    <ul>
-                      {reservationErrors.map((error, idx) => <li key={idx}>{error}</li>)}
-                    </ul>
-                  </>
-                )}
-              </> : <></> : <></>
+                        <>
+                          <ul>
+                            {reservationErrors.map((error, idx) => <li key={idx}>{error}</li>)}
+                          </ul>
+                        </>
+                      )}
+                    </> : <></> : <></>
                 }
-      </div>
-      )
+              </div>
+            )
           })}
-    </form>
+        </form>
 
       </div >
-  <div className="trips-right-div"></div>
+      <div className="trips-right-div"></div>
     </div >
   )
 }

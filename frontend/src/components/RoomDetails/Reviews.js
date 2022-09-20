a14a0608d3e29cf388d42133cba7f17a7d7e8fa1
lynnettenguyen <nguyelynne@gmail.com>
2022-09-19 21:51:57 -0700

    removed content if no reviews

--- frontend/src/components/RoomDetails/Reviews.js
+++ frontend/src/components/RoomDetails/Reviews.js
@@ -7,36 +7,37 @@ const Reviews = ({ room, avgStarRating }) => {
   const users = useSelector(state => state.users)
 
   return (
-    <div className="reviews-main">
-      <div className="reviews-header">
-        <i className="fa-solid fa-star reviews"></i>{avgStarRating}
-        <span className="span-separator-review">·</span>
-        <span>{room?.Reviews.length} reviews</span>
-      </div>
-      <div className="reviews-grid">
-        {users && room?.Reviews?.map((review, i) => {
-
-          let date = new Date(review?.createdAt)
-          const month = date.toLocaleString('default', { month: 'long' })
-          const year = date.getFullYear()
-          return (
-            <div className="reviews-outer">
-              <div className="reviews-upper">
-                <div className='review-profile-outer'>
-                  <img src={users[review?.userId]?.profile_url} className="review-profile-img" alt=""></img>
+    <>
+      {room?.Reviews.length > 0 ? <div className="reviews-main">
+          <div className="reviews-header">
+            <i className="fa-solid fa-star reviews"></i>{avgStarRating}
+            <span className="span-separator-review">·</span>
+            <span>{room?.Reviews.length} reviews</span>
+          </div>
+          <div className="reviews-grid">
+            {users && room?.Reviews?.map((review, i) => {
+              let date = new Date(review?.createdAt)
+              const month = date.toLocaleString('default', { month: 'long' })
+              const year = date.getFullYear()
+              return (
+                <div className="reviews-outer">
+                  <div className="reviews-upper">
+                    <div className='review-profile-outer'>
+                      <img src={users[review?.userId]?.profile_url} className="review-profile-img" alt=""></img>
+                    </div>
+                    <div className='review-user-outer'>
+                      <div className="review-first-name">{users[review?.userId]?.firstName}</div>
+                      <div className="review-date">{month} {year}</div>
+                    </div>
+                  </div>
+                  <div className="review-content">{review?.review}</div>
                 </div>
-                <div className='review-user-outer'>
-                  <div className="review-first-name">{users[review?.userId]?.firstName}</div>
-                  <div className="review-date">{month} {year}</div>
-                </div>
-              </div>
-              <div className="review-content">{review?.review}</div>
-            </div>
-          )
-        })}
-
-      </div>
-    </div >
+              )
+            })}
+          </div>
+        </div> : <></>
+      }
+    </>
   )
 }
 

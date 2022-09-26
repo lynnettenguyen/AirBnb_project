667c904b7639583f168b9f54f5a2edbebdc35890
lynnettenguyen <nguyelynne@gmail.com>
2022-09-26 14:17:32 -0700

    map icon

--- frontend/src/components/Maps/index.js
+++ frontend/src/components/Maps/index.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { GoogleMap, useJsApiLoader, Marker, Circle } from '@react-google-maps/api'
 import './Maps.css'
 import { getAPIKey } from '../../store/maps'
-import house from './house.svg'
+import house from './house.png'
 import mapOptions from './MapStyle'
 
 const Maps = ({ room }) => {
@@ -25,9 +25,15 @@ const Maps = ({ room }) => {
   };
 
   const center = {
-    lat: Number(room?.lat),
+    lat: Number(room?.lat + 0.023),
     lng: Number(room?.lng)
   };
+
+  const center2 = {
+    lat: Number(room?.lat + 0.02),
+    lng: Number(room?.lng)
+  };
+
 
   const circleOptions = {
     strokeColor: '#f3b2d0',
@@ -39,7 +45,7 @@ const Maps = ({ room }) => {
     draggable: false,
     editable: false,
     visible: true,
-    radius: 1000
+    radius: 1100
   }
 
   return (
@@ -56,14 +62,13 @@ const Maps = ({ room }) => {
                 styles: mapOptions,
                 streetViewControl: false,
                 mapTypeControl: false,
-                fullscreenControl: false,
-                scrollwheel: true
+                fullscreenControl: false
               }}>
               <Circle
                 center={center}
                 options={circleOptions}
               />
-              <Marker position={center} icon={house} />
+              <Marker position={center2} icon={house} />
             </GoogleMap>
           )}
       </div>

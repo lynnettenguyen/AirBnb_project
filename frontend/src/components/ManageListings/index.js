c95ee176989ab43b44f8bd115f30e31738caeb24
lynnettenguyen <nguyelynne@gmail.com>
2022-10-03 13:29:58 -0700

    added scroll to manage listings

--- frontend/src/components/ManageListings/index.js
+++ frontend/src/components/ManageListings/index.js
@@ -18,9 +18,9 @@ const ManageListings = ({ isLoaded }) => {
   }, [])
 
   const formatDate = (date) => {
-    let dateParts = date.slice(0,10).split("-");
+    let dateParts = date.slice(0, 10).split("-");
     let jsDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2].substr(0, 2));
-    return jsDate.toLocaleString('default', { month: 'short' }) + " " +  jsDate.getDate() + ", " + jsDate.getFullYear()
+    return jsDate.toLocaleString('default', { month: 'short' }) + " " + jsDate.getDate() + ", " + jsDate.getFullYear()
   }
 
   return (
@@ -32,42 +32,44 @@ const ManageListings = ({ isLoaded }) => {
       {sessionUser ?
         <div className="manage-listing-page">
           <div className="manage-listings-header">{userRooms?.length} listings</div>
-          <table className="manage-table-main">
-            <tr>
-              <th>LISTING</th>
-              <th>STATUS</th>
-              <th>PRICE</th>
-              <th>INSTANT BOOK</th>
-              <th>BEDROOMS</th>
-              <th>BEDS</th>
-              <th>BATHS</th>
-              <th>LOCATION</th>
-              <th>LAST MODIFIED</th>
-            </tr>
-            {userRooms?.map((room, i) => {
-              return (
-                <tr>
-                  <td className="listing-column">
-                    <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
-                      <img className="listing-img" src={`${room?.images[0]?.url}`} alt="preview of room"></img>
-                    </Link>
-                    <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
-                      <span className="listing-name">{room?.name}</span>
-                    </Link>
-                  </td>
-                  <td><img src={greenIcon} alt='listed' className="green-icon"></img>Listed</td>
-                  <td className="listing-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/night`}</td>
-                  <td><div className="listing-instant-book"><img src={greenCheck} alt='booking' className="green-checked"></img><span>On</span></div></td>
-                  <td>{room?.bedrooms}</td>
-                  <td>{room?.beds}</td>
-                  <td>{room?.baths}</td>
-                  <td className="listing-city-state">{`${room?.city}, ${room?.state}`}</td>
-                  <td>{formatDate(room?.updatedAt)}</td>
-                </tr>
-              )
-            })
+          <div className="manage-listings-scroll">
+            <table className="manage-table-main">
+              <tr>
+                <th>LISTING</th>
+                <th>STATUS</th>
+                <th>PRICE</th>
+                <th>INSTANT BOOK</th>
+                <th>BEDROOMS</th>
+                <th>BEDS</th>
+                <th>BATHS</th>
+                <th>LOCATION</th>
+                <th>LAST MODIFIED</th>
+              </tr>
+              {userRooms?.map((room, i) => {
+                return (
+                  <tr>
+                    <td className="listing-column">
+                      <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
+                        <img className="listing-img" src={`${room?.images[0]?.url}`} alt="preview of room"></img>
+                      </Link>
+                      <Link to={`/rooms/${room?.id}`} className="room-link" key={room?.id}>
+                        <span className="listing-name">{room?.name}</span>
+                      </Link>
+                    </td>
+                    <td><img src={greenIcon} alt='listed' className="green-icon"></img>Listed</td>
+                    <td className="listing-price">{`$${room?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/night`}</td>
+                    <td><div className="listing-instant-book"><img src={greenCheck} alt='booking' className="green-checked"></img><span>On</span></div></td>
+                    <td>{room?.bedrooms}</td>
+                    <td>{room?.beds}</td>
+                    <td>{room?.baths}</td>
+                    <td className="listing-city-state">{`${room?.city}, ${room?.state}`}</td>
+                    <td>{formatDate(room?.updatedAt)}</td>
+                  </tr>
+                )
+              })
             }
-          </table>
+            </table>
+          </div>
         </div> : <>
           <div className="no-session-user">
             Error 401 - Unauthorized
